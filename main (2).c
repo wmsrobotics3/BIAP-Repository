2fb9eecf45ec92632d141ca7b703c0013fff0e77
wmsrobotics3 <101908110+wmsrobotics3@users.noreply.github.com>
2023-03-18 11:39:19 -0700

    Our current code for our Roomba, 3.18.23

--- main (2).c
+++ main (2).c
@@ -1,77 +1,85 @@
 #include <kipr/wombat.h>
 #include <stdlib.h>
-/*cmpc(0); clears the position of the motor 0, or whatever port is in the ().
-This is important because if we don't clear it every time we use this function the wombat/wallaby will not move. */
+/*This means this function will accept two numbers, one is distance the other speed*/
 
-/*gmpc, or get_motor_position_counter checks the position of the motor */
+/* set_create_distance means the robot will set the number of registered ticks to 0. 
+This is important to if you were to make the robot move more than once.*/
 
-/*abs means that the robot will move in the way that we want it to*/
+/*On the next line, it says while the distance is less than the distance set, it will move until that point. */
 
-/*This means that the robot will keep moving, assuming that it's position is greater than the distance we set */
+/*The next two lines make sure the robot is accurate to your directions */
 
-/* mav stands for move at velocity. This is important because it tends to be more precise.
-values for mav is -1500 to 1500*/
+void drive/*name*/(int distance, int speed)
+{
+    set_create_distance(0);
+    while (abs(get_create_distance()) < distance)
+    {
+        create_drive_direct(speed,speed);
 
-void drive (int distance,int velocity){
- cmpc(0);
- while (abs(gmpc(0)) < distance){
-  mav(0,velocity);
-  mav(1,velocity+100);
- }
- mav(0,0);
- mav(1,0);
- msleep(34);
+    }
+    create_drive_direct(0,0);
+    msleep(34);
 }
 
 
-void turn_left (int distance,int velocity){
- cmpc(0);
- while (gmpc(0) < distance){
-  mav(0, velocity);
-  mav(1,-velocity);
- }
- mav(0,0);
- mav(1,0);
- msleep(34);
+/*create_drive_direct(-speed, speed) means the robot will mean that the robot's wheels will turn two different directions  */
+void turn_left(int distance, int speed){
+    set_create_total_angle(0);
+    while (abs(get_create_total_angle()) < distance){
+
+        create_drive_direct(-speed,speed);            
+    }
+
+    create_drive_direct(0,0);
+    msleep(34);
+
+}
+
+void turn_right(int distance,int speed){
+
+    set_create_total_angle(0);
+
+    while (abs(get_create_total_angle()) < distance){
+
+        create_drive_direct(speed,-speed);      
+    }
+    create_drive_direct(0,0);
+    msleep(34);
 }
 
 
-void turn_right (int distance,int velocity){
- cmpc(0);
- while (gmpc(0) < distance){
-  mav(0,-velocity);
-  mav(1, velocity);
- }
- mav(0,0);
- mav(1,0);
- msleep(34);
+/* get_create_rfcliff_amt refers to a sensor on the Create bot that can detect black and white. 
+Here, it says if the Create bot detects a color lower than 1690, a dark color, it will do the following */
+
+/**/
+
+void wait(int time)
+{
+    create_drive_direct(0,0);
+    msleep(time);
 }
 
 
-void line_follow(int distance, int velocity){
- cmpc(0);
- while (abs(gmpc(0)) < distance){
-  if (analog(0) < 1650){
-   mav(0,1000);
-   mav(1,500);
-  }
-  if (analog(0) > 1650){
-   mav(0,500);
-   mav(1,1000);
-  }
-   mav(0,0);
-   mav(1,0);
-   msleep(34);   
- }
+void line_follow(int distance, int speed){
+    set_create_distance(0);
+    while (abs(get_create_distance()) < distance){
+        if (get_create_rfcliff_amt() < 1690){
+            create_drive_direct(speed-25,speed+25);
+        }
+        if (get_create_rfcliff_amt() > 1690){
+            create_drive_direct(speed+25,speed-25);   
+        }
+    }
 }
-int arm_center = 1354;//arm is port 0
-int arm_left = 1770;
-int arm_right = 999;
+int claw_open = 524;
+int claw_close = 2047;
 
-int claw_close = 1500; // claw is port 1
-int claw_open = 1416;
- // port 0 is left
- // port 1 is right
+int arm_up = 1678;
+int arm_middle = 1035;
+int arm_down = 0;
+
+
+
 
 
 
@@ -79,15 +87,28 @@ int claw_open = 1416;
 
 int main()
 {
-	enable_servos();
-   	turn_left(980, 750);
-    drive(7200, 900);
-    mav(1, 1000);
-    mav(0, 700);
-    msleep(6000);
+    create_connect();
+    enable_servos();
+    set_servo_position(1,arm_up);
+    msleep(50);
+    set_servo_position(0,claw_close);
+    msleep(100);
+    drive(80,-90);
+    turn_right(90,50);
+    wait(100);
+    set_servo_position(1,arm_down);
+    msleep(1);
+    turn_left(1,50);
+    turn_left(64,50);
+    set_servo_position(0,claw_open);
+    drive(350,100);
+ 
    
-   
-   
-    
-    
+  
+
+
+
+
+    create_disconnect();
+    return 0;
 }
