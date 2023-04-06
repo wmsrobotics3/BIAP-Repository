bbe5cbaef4ba5a7cb99ff6496fea68d1d29a4cef
wmsrobotics3 <101908110+wmsrobotics3@users.noreply.github.com>
2023-04-06 15:22:11 -0700

    AUDREY
    made the code go to all three boxes

---
+++ program (3).c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <kipr/wombat.h>
+
+int main()
+{
+  int L=0;
+  int R=3;
+  enable_servo(0);
+
+set_servo_position(0,1000);
+ //drives into blue box
+ msleep(1000); 
+ 
+ motor(L,100);
+ motor(R,100);
+ msleep(5000);
+
+ motor(L,-100);
+ motor(R,100);
+ msleep(400);
+
+ motor(L,100);
+ motor(R,100);
+ msleep(1800);
+
+motor(L,0);
+motor(R,0);
+
+// make it go to orenge
+ set_servo_position(0,2000);
+
+ msleep(1000);
+
+ motor(L,-100);
+ motor(R,-100);
+ msleep(2000);
+
+ motor(L,100);
+ motor(R,-100);
+ msleep(400);
+
+ motor(L,100);
+ motor(R,-100);
+ msleep(300);
+
+ motor(L,100);
+ motor(R,100);
+ msleep(1500);
+ 
+ motor(L,100);
+ motor(R,100);
+ msleep(100);
+
+ motor(L,-100);
+ motor(R,100);
+ msleep(280);
+
+ motor(L,100);
+ motor(R,100);
+ msleep(120);
+
+ motor(L,-100);
+ motor(R,-100);
+ msleep(600);
+ 
+
+  printf("Hello, World!\n");
+  return 0;
+}
