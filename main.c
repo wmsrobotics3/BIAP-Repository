fbbb1fe684e94dad9fc9be761ce635c0fdf7aac4
wmsrobotics3 <101908110+wmsrobotics3@users.noreply.github.com>
2023-04-07 17:39:30 -0700

    Roomba Botball 4.7.23
    This is our updated version for the roomba V 1

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void drive (int distance,int velocity){
  cmpc(0);
  while (abs(gmpc(0)) < distance){
   mav(0,velocity);
-  mav(1,velocity+10);
+  mav(1,velocity+15);
  }
  mav(0,0);
  mav(1,0);
@@ -91,64 +91,90 @@ void servo( int port, int target_position, int pause_time)
       msleep(pause_time);
   }
 }
+int arm = 0;
 int arm_center = 320;//arm is port 0
 int arm_left = 784;
-int arm_right =10;
+int arm_right =0;
 
-int claw_close = 1262; // claw is port 1
-int claw_open = 1208;
+int claw = 1;
+int claw_close = 2047; // claw is port 1
+int claw_open = 0;
  // port 0 is left
  // port 1 is right
+int left_wheel = 0;
+int right_wheel = 1;
 
-
-
-
-
+int left = 1200;
+int right = 1200;
+int black_line = 1600;
+int white_line = 300;
 int main()
 {
 	enable_servos();
+   
     set_servo_position(1,claw_close);
     msleep(100);
-    turn_left(1200, 1000);//from starting box turning a 90* angle turn
-    servo(0,300,3.5);
+    servo(0,320,3.5);
+    turn_left(1150, 1000);//from starting box turning a 90* angle turn (step 1) 1240
     msleep(100);
     
-    drive(9350, 800);//drives foward for apporxiamtely 7300 ticks
+   drive(9400, 1000);//drives foward for apporxiamtely 9600 ticks(step 2)
     
-    turn_right(1100,6500);//turns right from roomba starting box
-    drive(1920,1000);//drives foward to align itself with the black line in order to line follow(facing foward)
-    while (analog(0) < 500)
-    {
-       mav(0, 650);
-       mav(1,-650);
-    }
+   turn_right(1070,650);//turns right from roomba starting box
+    
+   drive(1790,1000);//drives foward to align itself with the black line in order to line follow(facing foward)
+
+  
     mav(0,0);
     mav(1,0);
     msleep(34);
+    wait(100);
+    
+   drive(300,650);//300
+
+   turn_right(1100,1000);//turns right on the black line
    
-    //turn_left(260,100);
+ 
+   servo(arm,arm_right,1);
     
-    drive(1990, 1000); // meets second red pom 
-    servo(0,arm_left, 3.5); // prepares the arm to sort the green pom
+   drive(1900, 1000); // meets second red pom (1990)
+   servo(0,arm_left, 1.5); // prepares the arm to sort the green pom
     
-    drive(1990, 1000); // drives forward to collect green pom. 
-    servo(0, arm_right, 3.5); // prepares the arm to sort the red poms
-    
-    drive(1990, 1000); // meets second red pom 
-    servo(0,arm_left, 3.5); // prepares the arm to sort the green poms
-    
-    drive(1990, 1000); // drives forward to collect green pom. 
-    servo(0, arm_right, 3.5); // prepares the arm to sort the red pom
-        
-    turn_right(150,500);
-    
-    drive(1990, 1000); // meets second red pom 
-    servo(0,arm_left, 3.5); // prepares the arm to sort the green pom.     
+   drive(1900, 1000); // drives forward to collect green pom. 
+   servo(0, arm_right, 1.5); // prepares the arm to sort the red poms
+
+   drive(1900, 1000); // meets second red pom 
+   servo(0,arm_left, 1.5); // prepares the arm to sort the green poms
    
-    // the way the arm moves is in this order 
-    // straight, left, right, left, right, left, after is the two poms that need to be sorted.
+    /*while (analog(0) < white_line)
+    {
+     mav( right_wheel,-650);
+     mav(left_wheel,650);
+    }*/
+    //turn_left(20,650);
     
+   drive(1900, 1500); // drives forward to collect green pom. 
+   servo(0, arm_right, 1.5); // prepares the arm to sort the red pom
+   
+    turn_left(20,650);
+   
+   
+   drive(1900, 1000); // meets second red pom 
+   servo(0,arm_left, 1.5); // prepares the arm to sort the green pom.     
+   
+   servo(claw, claw_open, 0.1);
+   drive(1800, 650); // drives forward to collect green pom. 
+
+   wait(100);
     
+   servo(arm, 439 , 1);//576
+   drive(290,650);
+   wait(500);
+   drive(25,1000);
+   servo(claw, claw_close, 1);
+   set_servo_position(claw,claw_close);
+   msleep(100);
+   
    
    
     
